fa4566ff0af63cd4fddd53ca99bde1fc294217e7
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-06-04 15:48:37 +0900

    chore: app.serviceとcontroller削除

--- nest-ddd-practice2-ec/src/app.module.ts
+++ nest-ddd-practice2-ec/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RepositoryModule } from './infrastructure/repository/repository.module';
 
 @Module({
   imports: [RepositoryModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
