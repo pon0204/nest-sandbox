47ebacbb34c8bbd863b647862bea5f03a898e0f1
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-06-12 10:11:58 +0900

    feat: ApiProperty設定テスト

--- nest-open-api/src/cats/dto/create-cat.dto.ts
+++ nest-open-api/src/cats/dto/create-cat.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCatDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateCatDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  age: number;
+
+  @ApiProperty()
+  breed: string;
+}
