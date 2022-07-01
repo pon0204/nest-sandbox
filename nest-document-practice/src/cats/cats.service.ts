4b0a228dd2e3de7c9a3aa041250b3b45dc95e1ad
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-07-01 18:41:58 +0900

    feat: 循環依存について

--- nest-document-practice/src/cats/cats.service.ts
+++ nest-document-practice/src/cats/cats.service.ts
@@ -4,6 +4,11 @@ import { Cat } from './interfaces/cat.interface';
 // @Injectable({ scope: Scope.REQUEST }) // リクエストのたびにインスタンスを作製する。
 @Injectable()
 export class CatsService {
+  // 循環依存の解決
+  // constructor(
+  // @Inject(forwardRef(() => CommonService))
+  // private commonService: CommonService,
+  // ) {}
   private readonly cats: Cat[] = [];
   private readonly cat: Cat = {} as Cat;
 
