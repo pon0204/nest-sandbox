c39986eab033417f89976e736ce634b94682ebc5
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-07-02 20:34:05 +0900

    feat: @Exposeによるプロバティの追加

--- nest-document-practice/src/cats/dto/cat-res.dto.ts
+++ nest-document-practice/src/cats/dto/cat-res.dto.ts
@@ -1,13 +1,18 @@
-import { Transform } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 
 export class CatRes {
   constructor(name: string, age: number, breed: string) {
     (this.name = name), (this.age = age), (this.breed = breed);
   }
-  @Transform(({ value, key }) => '名前上書き')
+  // @Transform(({ value, key }) => '名前上書き')
   name: string;
   age: number;
   breed: string;
+
+  @Expose()
+  get profile(): string {
+    return `名前: ${this.name} 年齢:${this.age}歳 種類:${this.breed}`;
+  }
 }
 
 // @Transform(({ value, key, obj, type }) => value)
