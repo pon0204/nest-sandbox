81b149141edfb45e39b0e3c4d1be0da207c5128a
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-07-09 12:20:24 +0900

    feat: userServiceのcreateテスト実装

--- nest-test-practice/src/user/user.service.spec.ts
+++ nest-test-practice/src/user/user.service.spec.ts
@@ -2,10 +2,18 @@ import { UserService } from './user.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersService', () => {
   let service: UserService;
-  const mockUsersRepository = {};
+  const mockUsersRepository = {
+    create: jest.fn().mockImplementation((dto) => dto),
+    save: jest
+      .fn()
+      .mockImplementation((user) =>
+        Promise.resolve({ id: Date.now(), ...user }),
+      ),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,5 +32,18 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new user record and return that', async () => {
+    const dto: CreateUserDto = {
+      firstName: 'つよし',
+      lastName: 'いとう',
+      isActive: true,
+    };
+
+    expect(await service.create(dto)).toEqual({
+      id: expect.any(Number),
+      ...dto,
+    });
   });
 });
